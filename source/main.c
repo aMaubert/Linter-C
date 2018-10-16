cda7739fda84b59439a496eb7bfacf3d00a2936a
Allan-MAUBERT <a.maubert.jouvet@gmail.com>
2018-10-16 19:21:01 +0200

    added the subject file

---
+++ source/main.c
@@ -0,0 +1,15 @@
+/**
+ *  author : amaubert
+ *  date : 2018/10/18
+ *  version : 1.0
+ *  file : source/main.c
+ ************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+
+  printf("coucou\n") ;
+  return 0;
+}
