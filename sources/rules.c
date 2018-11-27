2c42e447a13db51e2de00ce5f2b200e040260bae
Myst-Click <guillaumecassoly@hotmail.fr>
2018-11-27 12:16:11 +0100

    regle espace entre operateur

--- sources/rules.c
+++ sources/rules.c
@@ -18,70 +18,58 @@
 #define __SIZE_LINE__ 128
 #define __SIZE_BUFFER__ 5000
 
-
-void array_bracket_eol(FILE* f){
-  f = fopen("C:\\Users\\Ordinateur\\Desktop\\projet\\test\\test.c", "r");
-  if( f != NULL){
-    printf("Fichier bien ouvert\n");
+void operators_spacing(FILE* f){
+    printf("------------------------------\n");
+    printf("Regle Espace Opérateur :\n\n");
+    int line = 1;
     char Currentligne[258];
-    char *Alllignes[258];
-    char functions[][10] = {"if","while","for","do","void","int"};
-    //char *lignes;
+    bool Iscomm = false;
+    int errorOperator = 0;
+    //fonction typeof
 
-
-    //while(c)
-int j = 1;
-bool Iscomm = false;
-   while(fgets(Currentligne, sizeof(Currentligne), f) != NULL){
-      Alllignes[j] = Currentligne;
-        for(int i =0; i < strlen(Currentligne); i++){
-
-        if(Iscomm == true)        //Si commentaire
-        {
-          while ((Currentligne[i] != '*' && Currentligne[i+1] != '/') && i <= strlen(Currentligne))
+       while(fgets(Currentligne, sizeof(Currentligne), f) != NULL){
+         for(int i = 0; i < strlen(Currentligne); i++){
+          if(Currentligne[i] == '/' && Currentligne[i+1] == '*')
           {
-            i++;
+           Iscomm = true;
+           i = i + 2;
           }
 
-          if(Currentligne[i] == '*' && Currentligne[i+1] == '/'){       //Sortie commentaire
-            Iscomm = false;
-          }
-
-        }
-
-        else if(Currentligne[i] == '/' && Currentligne[i+1] == '*')       // Si commentaire
-        {
-          Iscomm = true;
-          i = i+2;
-
-          while ((Currentligne[i] != '*' && Currentligne[i+1] != '/') && i < strlen(Currentligne)) // Tan quon est dans le commentaire
+          if(Iscomm == true)        //Si commentaire
           {
-            i++;
-          }
-          if(Currentligne[i-1] == '*' && Currentligne[i] == '/')                      // Si sortie du commentaire
-          {
-            Iscomm = false;
-          }
-          else Iscomm = true;
-        }
-        int k = 0;
-        while(strstr(Currentligne,functions[k]) == NULL && k < 5)
-        {
-          k++;
-        }
-        if((strstr(Currentligne,functions[k]) != NULL) && Iscomm == false)          // Si fonction presente
-        {
-             if(Currentligne[i] == '{' && i != (strlen(Currentligne)-2)){           // Si accolade presente sur la ligne
-              printf("Erreur d'accolade ligne %d \n \n", j);
+            while ((Currentligne[i] != '*' && Currentligne[i+1] != '/') && i <= strlen(Currentligne))
+            {
+              i++;
             }
-        }
-      else  if(Currentligne[i] == '{') printf("Erreur d'accolade ligne %d \n \n", j); // Si accolade pas sur ligne fonction
-      }
-      j++;
 
-    }
-    printf("Lors de l'appel d'une fonction,\nl'accolade doit etre precédée de la fonction et suivi du retour à la ligne.");
+            if(Currentligne[i] == '*' && Currentligne[i+1] == '/'){    //Sortie commentaire
+            Iscomm = false;
+            i++;
+            }
+          }
+          int erreur = 0;
+          if(Currentligne[i] == '=' && Iscomm == false){
+            
+              if(Currentligne[i+1] != ' '){
+                erreur ++;
+               }
+              if(Currentligne[i-1] == '+' |Currentligne[i-1] == '-'|Currentligne[i-1] == '*'|Currentligne[i-1] == '/' ){
+                if(Currentligne[i-2] != ' '){
+                  erreur ++;
+                 }
+              }
+              else if(Currentligne[i-1] != ' '){
+                erreur ++;
+              }
+          }
+          if(erreur > 0){
+              printf("%d erreur(s) de syntaxe à la ligne %d\n", erreur, line);
+              errorOperator++;
+          } 
+         }
 
-  }
-    fclose(f);
-  }
+         line ++;
+       }
+       rewind(f);
+       if(errorOperator == 0) printf("Aucune erreur de Syntaxe.\n"); 
+}
