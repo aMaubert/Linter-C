3f752c3d4c1a00ac9e440c17f8684363b733b582
Myst-Click <guillaumecassoly@hotmail.fr>
2018-11-27 12:26:32 +0100

    regle nombre de ligne maximum

--- sources/rules.c
+++ sources/rules.c
@@ -19,46 +19,24 @@
 #define __SIZE_BUFFER__ 5000
 
 
-void comments_header(FILE* f){
-    printf("------------------------------\n");
-    printf("Regle Présence d'un commentaire multi-ligne:\n\n");
-    int line = 1;
-    int afterComm = 0;
-    int isCommBefore = 0;
-    char Currentligne[258];
-    bool Iscomm = false;
-    //fonction typeof
+void nbLine (int nb,FILE * f){
+  printf("------------------------------\n");
+  printf("Regle Nombre de ligne maximum dans un fichier :\n\n");
+  int counter = 0 ;
 
-       while(fgets(Currentligne, sizeof(Currentligne), f) != NULL){
-        int erreur = 0;
-         for(int i = 0; i < strlen(Currentligne); i++){
-          if(Currentligne[i] == '/' && Currentligne[i+1] == '*' && line == 1)
-          {
-            isCommBefore = 1;
-           Iscomm = true;
-           i = i + 2;
-          }
-
-          if(Iscomm == true)        //Si commentaire
-          {
-            while ((Currentligne[i] != '*' && Currentligne[i+1] != '/') && i <= strlen(Currentligne))
-            {
-              i++;
-            }
-
-            if(Currentligne[i] == '*' && Currentligne[i+1] == '/'){    //Sortie commentaire
-            Iscomm = false;
-            i++;
-            }
-          }
-          if(Iscomm == false && isCommBefore == 1)
-          {
-            if(Currentligne[i] != ' ') afterComm ++;
-          }
-        }
-        line ++;
-      }
-      rewind(f);
-      if(afterComm > 0) printf("La présence d'un commentaire multi-ligne en entete de fichier a été confirmé.\n");
-      else printf("La présence d'un commentaire multi-ligne en entete de fichier n'a pas été confirmé.\n");
+  char CurrentLine[248];
+  int dif=0;
+  while(fgets(CurrentLine, sizeof(CurrentLine), f) !=NULL){
+      counter++;
+  }
+  if(counter > nb)
+    {
+      dif = counter - nb;
+      printf("Nombre de ligne dépassé : %d ligne(s) de trop. \n", dif);
+    }
+  else
+    {
+      printf("Le nombre de ligne est correct. \n");
+    }
+    rewind(f);
 }
