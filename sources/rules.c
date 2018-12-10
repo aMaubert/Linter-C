dec5b0ae68e6cf767494aef762fd5f1ecc764cef
Myst-Click <guillaumecassoly@hotmail.fr>
2018-12-10 16:53:20 +0100

    operator_spacing

--- sources/rules.c
+++ sources/rules.c
@@ -18,71 +18,95 @@
 #define __SIZE_LINE__ 128
 #define __SIZE_BUFFER__ 5000
 
-void array_bracket_eol(FILE* f){
-    printf("------------------------------\n");
-    printf("Regle Accolade :\n\n");
-    char Currentligne[258];
-    char *Alllignes[258];
-    char functions[][10] = {"if","while","for","do","void","int"};
-    //char *lignes;
-
-
-    //while(c)
-    int errorBracket = 0;
-    int j = 1;
-    bool Iscomm = false;
-     while(fgets(Currentligne, sizeof(Currentligne), f) != NULL){
-        Alllignes[j] = Currentligne;
-          for(int i =0; i < strlen(Currentligne); i++){
-
-          if(Iscomm == true)        //Si commentaire
-          {
-            while ((Currentligne[i] != '*' && Currentligne[i+1] != '/') && i <= strlen(Currentligne))
-            {
-              i++;
-            }
-
-            if(Currentligne[i] == '*' && Currentligne[i+1] == '/'){       //Sortie commentaire
-              Iscomm = false;
-            }
-
-          }
-
-          else if(Currentligne[i] == '/' && Currentligne[i+1] == '*')       // Si commentaire
-          {
-            Iscomm = true;
-            i = i+2;
-
-            while ((Currentligne[i] != '*' && Currentligne[i+1] != '/') && i < strlen(Currentligne)) // Tan quon est dans le commentaire
-            {
-              i++;
-            }
-            if(Currentligne[i-1] == '*' && Currentligne[i] == '/')                      // Si sortie du commentaire
-            {
-              Iscomm = false;
-            }
-            else Iscomm = true;
-          }
-          int k = 0;
-          while(strstr(Currentligne,functions[k]) == NULL && k < 5)
-          {
-            k++;
-          }
-          if((strstr(Currentligne,functions[k]) != NULL) && Iscomm == false)          // Si fonction presente
-          {
-               if(Currentligne[i] == '{' && i != (strlen(Currentligne)-2)){           // Si accolade presente sur la ligne
-                errorBracket++;
-                printf("Erreur d'accolade à la ligne %d \n \n", j);
-              }
-          }
-        else  if(Currentligne[i] == '{'){
-          printf("Erreur d'accolade à la ligne %d \n \n", j); // Si accolade pas sur ligne fonction
-          errorBracket++;
-        }
-        }
-        j++;
-
+void operatorsSpacingRule(FILE * f){
+  int line = 1;
+  char CurrentLine[258];
+  char type[][100]={"FILE","int","char","float","short","double","long"};
+  bool Iscomm = false;
+  int errorOperator = 0;
+   while(fgets(CurrentLine, sizeof(CurrentLine), f) != NULL){
+    for(int i = 0; i < strlen(CurrentLine); i++){
+      if(CurrentLine[i] == '/' && CurrentLine[i+1] == '/'){
+        fgets(CurrentLine, sizeof(CurrentLine), f);
+          line++;
       }
-      rewind(f);
-      if(errorBracket == 0)printf("Aucune erreur de syntaxe.\n");
+      if(CurrentLine[i] == '/' && CurrentLine[i+1] == '*')
+      {
+        Iscomm = true;
+        i = i + 2;
+      }
+       if(Iscomm == true)        //Si commentaire
+      {
+        while ((CurrentLine[i] != '*' && CurrentLine[i+1] != '/') && i <= strlen(CurrentLine))
+        {
+          i++;
+        }
+         if(CurrentLine[i] == '*' && CurrentLine[i+1] == '/'){    //Sortie commentaire
+          fgets(CurrentLine, sizeof(CurrentLine), f);
+          line++;
+          Iscomm = false;
+          i++;
+        }
+      }
+      int erreur = 0;
+      /* Test equals operator */
+      if(CurrentLine[i] == '=' && Iscomm == false){
+         if((CurrentLine[i+1] != ' ' && CurrentLine[i+1] != '=')||(CurrentLine[i-1] != ' '&& CurrentLine[i-1] != '='&& CurrentLine[i-1] != '-'&& CurrentLine[i-1] != '+'&& CurrentLine[i-1] != '/'&& CurrentLine[i-1] != '*'&& CurrentLine[i-1] != '%')){
+          erreur ++;
+        }
+      }
+      /* Test addition operator */
+      else if((CurrentLine[i] == '+' && Iscomm == false)&&(CurrentLine[i-1]!='+') && (CurrentLine[i+1]!='+')){
+            if((CurrentLine[i+1] != ' ' && CurrentLine[i+1] != '=') ||(CurrentLine[i-1] != ' ')){
+                    erreur++;
+            }
+      }
+     /* Test soustraction operator */
+      else if ((CurrentLine[i] == '-' && Iscomm == false)&&(CurrentLine[i-1]!='-') && (CurrentLine[i+1]!='-')){
+        if((CurrentLine[i+1] != ' ' && CurrentLine[i+1] != '=')||(CurrentLine[i-1] != ' ')){
+            erreur++;
+        }
+      }
+      /* Test ultiplication and division operator */
+      else if ((CurrentLine[i] == '*' && Iscomm == false)||(CurrentLine[i] == '/' && Iscomm == false)||(CurrentLine[i] == '%' && Iscomm == false)){
+            int isTypeBefore = 0;
+            if(CurrentLine[i] == '*'){
+              char beforeStar[258];
+              int t = i-1;
+              int nbCharacteres = 0;
+              while((CurrentLine[t] != ' ' & CurrentLine[t] != '(')&& t > 0){
+                nbCharacteres++;
+                t--;
+              }
+              if( t == 0) nbCharacteres++;
+              if(CurrentLine[t] == ' ') t++;
+
+              beforeStar[nbCharacteres] = '\0';
+              for(int k = 0; k < nbCharacteres; k ++){
+                beforeStar[k] = CurrentLine[t+k];
+              }
+
+              for(int j=0;j < 6;j++){
+
+               if(strcmp(beforeStar,type[j])){
+                  isTypeBefore++;
+                } 
+                
+              }
+            }
+            if((CurrentLine[i+1] != ' ' && CurrentLine[i+1] != '=')||(CurrentLine[i-1] != ' ') && isTypeBefore == 0){
+                erreur++ ;
+            }
+      }
+      if(erreur > 0){
+        char message[512] ;
+        printf("Erreur de syntaxe : %d erreur(s) ligne %d\n", erreur, line);
+       // sprintf(message, "Regle : Operators_Spacing\nIl y a %d erreur(s) de syntaxe à la ligne %d du fichier  .\n", erreur, line) ;
+        //messageLog(logger, message) ;
+        errorOperator++;
+      }
+    }
+     line ++;
+  }
+  rewind(f);
 }
